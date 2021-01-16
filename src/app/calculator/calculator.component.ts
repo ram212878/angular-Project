69b53616b01f76167f05da862b94153e4b030091
ram212878 <ramk212878@gmail.com>
2021-01-16 11:57:54 +0530

    Buttons added , click event for button also added

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,9 +6,39 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./calculator.component.css"]
 })
 export class CalculatorComponent implements OnInit {
-  constructor() {}
   result = "";
+  value = "";
+  keys = [];
+  constructor() {}
+
   ngOnInit() {
     this.result = "0";
+    this.keys = [
+      "-",
+      "+",
+      "/",
+      "*",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "=",
+      "C"
+    ];
+  }
+
+  text(val) {
+    this.value += val;
+    this.result = this.value;
+  }
+
+  calculate() {
+    this.result = eval(this.result);
   }
 }
